2a9c2cba5156f41ffc657bc3d66fa92d49c36735
red0orange <20183231102@m.scnu.edu.cn>
2022-04-07 20:29:16 +0800

    ✨ feat: 1. 改为使用OpenCV4.0.0版本，修改了一些头文件 2. 修复mesh的bug 3. 修复TUM.yaml读取的bug

---
+++ include/Config.h
@@ -0,0 +1,33 @@
+//
+// Created by fishmarch on 19-5-24.
+//
+
+#ifndef ORB_SLAM2_CONFIG_H
+#define ORB_SLAM2_CONFIG_H
+#include <opencv2/core/core.hpp>
+#include <memory>
+#include <iostream>
+
+using namespace std;
+namespace Planar_SLAM {
+    class Config{
+    public:
+        static void SetParameterFile( const string& filename );
+
+        template <typename T>
+        static T Get(const string& key){
+            // cv::FileStorage file_storage = cv::FileStorage("/home/hdh6/github_projects/PlanarSLAM/Examples/RGB-D/TUM3.yaml", cv::FileStorage::READ);
+            // T result = T(file_storage[key]);
+            // file_storage.release();
+            // return result;
+            return T(Config::mConfig->mFile[key]);
+        }
+        ~Config();
+    private:
+        Config(){}
+        static std::shared_ptr<Config> mConfig;
+        cv::FileStorage mFile;
+        std::string filename;
+    };
+}
+#endif //ORB_SLAM2_CONFIG_H
